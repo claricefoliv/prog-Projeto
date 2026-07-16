3aa8ebe7a8d2dea947f464cecccd3793e395634d
clarice lopes <claricefoliv@gmail.com>
2026-07-16 02:29:57 -0300

    Adicionando Mover e Lógica de Camadas, mantendo o modelo MVC - Etapa 5
    Atendendo aos requisitos de modelo MVC e implementação de Mover e Mover figuras em camadas, exigidos pelo professor orientador

--- src/paint/modelo/figuras.py
+++ src/paint/modelo/figuras.py
@@ -1,3 +1,4 @@
+
 # a parte de modelo da organização MVC transforma o projeto ao incrementar a parte visual e funcional dos arquivos, transmitindo o que foi feito de forma mais clara.
 ## para o model, primeira etapa da organização, retomamos o arquivo que antes era denominado "Figura.py" e o transformamos no modelo.
 ### nesse caso, o modelo funciona como o _armazenamento de dados_, ou seja, lida apenas com a denominação da figura a ser criada.
@@ -28,7 +29,7 @@ class Figuras:
     ## dentro da propria classe mãe que reconstroi qualquer subclasse de uma forma "padrao" e generaliza os atributos comuns entre si, facilitando não só o entendimento mas também a clareza
     ## do codigo
     @classmethod
-    def from_dict(cls, dados):
+    def from_dict(cls, dados: dict):
         # aqui, se for rabisco ou poligono, as coordenadas devem ser mantidas como listas
         if cls.__name__ in ("Rabisco", "Poligono"):
             coords = dados["coordenadas"]
@@ -60,14 +61,54 @@ class Figuras:
             cor_borda=self.cor_borda,
             cor_preenchimento=self.cor_preenchimento
         )
+    
+    #################### Mudança - Etapa 5 na parte de mover as figuras! #################
+    ## a nossa primeira ideia aqui era de fato implementar a logica de deslocar as figuras pelo model, mas isso iria contra a ideia de que estamos apenas armazenando dados.
+    ## desse modo, tiramos esse codigo e jogamos ele para o estado, respeitando o modelo mvc.
+    '''
+    def deslocar(self, dx, dy):
+        # move as coordenadas da figura somando uma variação dx e dy: se o clique foi "dentro" dos limites de alguma figura, nós a selecionamos e guardamos onde o clique começou
+        if isinstance(self.coordenadas, tuple):
+            # para linha, oval e retangulo que usam tuplas de 4 pontos: x1, y1, x2, y2
+            x1, y1, x2, y2 = self.coordenadas
+            self.coordenadas = (x1 + dx, y1 + dy, x2 + dx, y2 + dy)
+        else:
+            # para rabisco e poligono que usam listas de pontos [(x,y), ...]
+            self.coordenadas = [(x + dx, y + dy) for x, y in self.coordenadas]
 
+    def contem_ponto(self, px, py):
+        # verifica se o clique (px, py) ocorreu dentro dos limites da figura
+        if isinstance(self.coordenadas, tuple):
+            x1, y1, x2, y2 = self.coordenadas
+            min_x = min(x1, x2)
+            max_x = max(x1, x2)
+            min_y = min(y1, y2)
+            max_y = max(y1, y2)
+        else:
+            # para rabisco e polígono, percorre todos os vértices ate achar as extremidades
+            todos_x = [ponto[0] for ponto in self.coordenadas]
+            todos_y = [ponto[1] for ponto in self.coordenadas]
+            min_x = min(todos_x)
+            max_x = max(todos_x)
+            min_y = min(todos_y)
+            max_y = max(todos_y)
+        
+        # retorna true se o clique estiver dentro das extremidades. esse 5 é para criar uma margem de 5 pixels
+        return (min_x - 5 <= px <= max_x + 5) and (min_y - 5 <= py <= max_y + 5)
+        '''
+
+    ## a funcao _copiar_coordenadas é a unica que permanece no figuras pois precisamos dela para duplicar os dados! ainda permanecemos delegando somente as funcoes do model para o model.
     def _copiar_coordenadas(self):
-        # Rabisco e Poligono usam lista de tuplas -> copiamos a lista
+        # rabisco e poligono usam lista de tuplas e ai copiamos a lista
         if isinstance(self.coordenadas, list):
             return [tuple(p) for p in self.coordenadas]
-        # Linha, Oval, Retangulo usam tupla de 4 números -> copiamos a tupla
+        # linha, oval e retangulo usam tupla de 4 números e tambem copiamos a tupla
         else:
             return tuple(self.coordenadas)
+        
+################### Fim das mudanças de mover a figura no model #########################
+
+
 # permanecem o mesmo
 class Linha(Figuras):
     pass
@@ -107,7 +148,7 @@ class Desenho:
         self.figura_nova = None
 
     ## aqui colocamos as modificacoes para salvar e abrir JSON!!
-    def salvar_para_arquivo(self, caminho_arquivo):
+    def salvar_para_arquivo(self, caminho_arquivo: str) -> None:
         # converte todas as figuras salvas para dicionário atraves do to_dict()
         dados_figuras = [fig.to_dict() for fig in self.figuras]
         # aqui é fundamental para guardar as informacoes em um arquivo JSON: abre o caminho no sistema operacional para um arquivo, denomina como modo de escrita através do w (que significa
@@ -116,7 +157,7 @@ class Desenho:
             # o indent serve para formatacao do arquivo e o dump serve para fazer a conversao de dados em JSON.
             json.dump(dados_figuras, arquivo, indent=4)
 
-    def carregar_de_arquivo(self, caminho_arquivo):
+    def carregar_de_arquivo(self, caminho_arquivo: str) -> None:
         # lê (o r ali significa read) o arquivo JSON e carrega os objetos de figuras da mesma maneira que testado anteriormente na tela
         with open(caminho_arquivo, "r", encoding="utf-8") as arquivo:
             dados_figuras = json.load(arquivo)
@@ -139,3 +180,6 @@ class Desenho:
                 # usamos o método herdado from_dict para criar o objeto correto, utilizando o dicionario como manual de instrucoes
                 figura_reconstruida = classe_figura.from_dict(dados)
                 self.adicionar_figura(figura_reconstruida)
+
+
+
