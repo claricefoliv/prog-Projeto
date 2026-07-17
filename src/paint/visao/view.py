bbb94f49f958909f70e3499daeb252a917a6ef8a
clarice lopes <claricefoliv@gmail.com>
2026-07-16 21:16:55 -0300

    Ultimo update do view para a etapa 5!
    Renomeamos os nomes das funções que alteram as cores da borda e preenchimento de figuras.

--- src/paint/visao/view.py
+++ src/paint/visao/view.py
@@ -156,23 +156,17 @@ class TelaDesenho:
             self.btn_mover.config(relief=tk.SUNKEN, bg="#ddd")
 ## Etapa finalizada!!!
 
-    def _set_cor_borda(self, cor):
-        """Atualiza a cor da borda futura e altera a cor da figura atualmente focada, se houver."""
-        self.cor_borda = cor
-        
-        # Se o usuário clicou em uma figura com a ferramenta "Mover" (focando nela), mudamos a cor dela
-        if self.controlador.figura_focada:
-            self.controlador.figura_focada.cor_borda = cor
-            self.controlador._redesenhar()
+###### MUDANÇA AQUI!!!!! ETAPA 5: ALTERAMOS NOVAMENTE PARA _MUDAR_COR_BORDA ##### 
+    ## agora conseguimos mudar a cor de todas as figuras!
+    def _mudar_cor_borda(self, valor: str) -> None:
+        # avisa o controlador que o usuário escolheu uma nova cor de borda
+        if self.controlador:
+            self.controlador.set_cor_borda(valor)
 
-    def _set_cor_preenchimento(self, cor):
-        """Atualiza a cor de preenchimento futura e altera a cor da figura atualmente focada, se houver."""
-        self.cor_preenchimento = cor
-        
-        # Se o usuário clicou em uma figura com a ferramenta "Mover" (focando nela), mudamos a cor dela
-        if self.controlador.figura_focada:
-            self.controlador.figura_focada.cor_preenchimento = cor
-            self.controlador._redesenhar()
+    def _mudar_preenchimento(self, valor: str) -> None:
+        # avisa o controlador que o usuário escolheu um novo preenchimento.
+        if self.controlador:
+            self.controlador.set_cor_preenchimento(valor)
 
     # Eventos do mouse
 
